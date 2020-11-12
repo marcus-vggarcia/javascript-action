5baa91ef01f128c115d5beb3e47f64d9dc9dc150
Marcus <marcus.garcia@ebanx.com>
2020-11-12 11:39:49 -0300

    Trying to release

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const fs = require("fs");
 
+var file = './search-data.json'; //(with path)
+var fileUrl = './restricted-urls.json'; //(with path)
 
+var filex = './search-data-x.json'; //(with path)
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
-
+    core.info(`Opening search-data.json file ...`);
+  
+    let data = fs.readFileSync(file, 'utf8');
+    let obj = JSON.parse(data);
+    let root = obj;
     
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.info('\x1b[0m',` `);
+    core.info(`Opening URL list file ...`);
 
-    core.setOutput('time', new Date().toTimeString());
+    let urlList = fs.readFileSync(fileUrl, 'utf8');
+    let urls = JSON.parse(urlList);
+    
+    core.info(` `);
+    core.info("Urls to be removed: " + urls)
+
+    for (var n = 0; n< urls.length; n++) {
+
+      let url = urls[n];
+
+      core.info (" > Removing " + url);
+      for (var i = 0; i < root.length; i++){
+        // look for the entry with a matching `code` value
+        if (root[i] && root[i].url.includes(url)){          
+          core.info ("   > URL" + root[i].url + " with index " + i + " removed")  ;
+          delete root[i]
+        }
+      }
+    
+    }
+      
+    data = JSON.stringify(root, null, 2);
+    fs.writeFileSync(file, data, 'utf8');
+        
+    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true    
+    core.info((new Date()).toTimeString());
+    
   } catch (error) {
     core.setFailed(error.message);
   }
